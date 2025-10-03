7e51992c642b292ecee5bca1d5c27a3aefc5ae06
nevmstas <nevmstas@users.noreply.github.com>
2025-10-03 12:29:06 +0000

    deploy: 42b4c55c2dcd9980272bed49bd5aff27ef291e1a

--- _next/static/chunks/app/layout-32acb84ae1039607.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3139:()=>{},6572:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}},8757:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,3139,23)),Promise.resolve().then(l.t.bind(l,6572,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[779,587,315,358],()=>s(8757)),_N_E=e.O()}]);
